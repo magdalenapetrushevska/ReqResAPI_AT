a7e2aee3af182dca39105048027ba317cc3740cb
magdalenapetrushevska <magdalenapetrusevska9@gmail.com>
2024-11-21 22:16:33 +0100

    implement test login method

--- src/test/java/APITests.java
+++ src/test/java/APITests.java
@@ -1,3 +1,13 @@
+/*
+ ******************************************************************************************************************************************
+ * Description: This file contains script for testing different test cases related to ReqRes endpoints
+ * Author: Magdalena Petrushevska
+ * Created_at: 19.11.2024
+ * Updated_at:
+ * Updated by:
+ */
+
+
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
@@ -6,6 +16,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.notNullValue;
 
 public class APITests {
 
@@ -55,7 +66,7 @@ public class APITests {
         Assertions.assertEquals(200, response.statusCode());
     }
 
-    // GET resources by page number
+    // GET resources by id
     @Test
     public void getResourceById() {
         Response response = given()
@@ -78,11 +89,23 @@ public class APITests {
                 .queryParam("id",1)
                 .when()
                 .get("/users")
-
                 .then()
                 .extract().response();
 
         Assertions.assertEquals(200, response.statusCode());
+    }
+
+    //POST login method
+    @Test
+    public void loginToken() {
+        given()
+                .contentType(ContentType.JSON)
+                .body("{\"username\": \"george.bluth@reqres.in\", \"email\": \"george.bluth@reqres.in\",\"password\": \"test123\"}")
+                .when()
+                .post("/login")
+                .then()
+                .statusCode(200)
+                .body("token", notNullValue());
     }
 
 
